2229a291bd819342ca907877b5b7803bc49e65bd
Josiah Connors <josiah.connors23@outlook.com>
2024-01-29 16:44:11 -0500

    init account struct

--- katas/banking.go
+++ katas/banking.go
@@ -1,13 +1,24 @@
-// implement stack with slices as additional feature
+// implement stack
 
 /*
 
-Write a class Account that offers the following methods void deposit(int) void withdraw(int) String printStatement()
+Write a class Account that offers the following methods:
+void deposit(int, name)
+void withdraw(int, name)
+string List(name) - lists transactions in most recent order when given name
 
 An example statement would be:
 
 Date        Amount  Balance
 24.12.2015   +500      500
 23.8.2016    -100      400
-/ LS
-*/ 
+
+*/
+
+package main
+
+type Account struct {
+	name         string
+	balance      float64
+	transactions map[string]int
+}
