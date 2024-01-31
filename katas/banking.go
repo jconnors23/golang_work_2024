fc915c7a852d332e366a74b8b911c6572db5675d
Josiah Connors <josiah.connors23@outlook.com>
2024-01-30 23:02:08 -0500

    reversed format of transactions map

--- katas/banking.go
+++ katas/banking.go
@@ -25,7 +25,7 @@ import (
 type Account struct {
 	name         string
 	balance      float64
-	transactions map[time.Time]float64
+	transactions map[string]float64
 }
 
 func (a *Account) withdraw(amount float64) {
@@ -33,22 +33,28 @@ func (a *Account) withdraw(amount float64) {
 		fmt.Println("account has insufficient funds.")
 		os.Exit(0)
 	}
-	a.transactions[time.Now()] = float64(amount)
+	a.transactions[time.Now().Format("01/01/2024 00:00:00")] = float64(amount)
 }
 
 func (a *Account) deposit(amount float64) {
 	a.balance += amount
-	a.transactions[time.Now()] = float64(amount)
+	a.transactions[time.Now().Format("01/01/2024 00:00:00")] = float64(amount)
 }
 
 // transaction map is inherently unsorted, using a sort method to implement stack behavior
 func (a *Account) stack_transactions() []time.Time {
 	lifo := make([]time.Time, 0, len(a.transactions))
+	timeLayout := "01/01/2024 00:00:00"
 	for transaction := range a.transactions {
-		lifo = append(lifo, transaction)
+		parsedTime, err := time.Parse(timeLayout, transaction)
+		if err != nil {
+			fmt.Println("time parsing error", err)
+			os.Exit(0)
+		}
+		lifo = append(lifo, parsedTime)
 	}
 	sort.Slice(lifo, func(i, j int) bool {
-		return lifo[i].After(lifo[j])
+		return lifo[i].After(lifo[j]) // will sort with most recent transactions at top
 	})
 	return lifo
 }
@@ -60,10 +66,14 @@ func (a *Account) list() {
 	}
 }
 
-// func (a *Account) list() map[string]int {
-// 	return
-// }
-
 func main_bank() {
-	fmt.Println()
+	t := Account{
+		name:    "test",
+		balance: 1000,
+		transactions: map[string]float64{
+			"24.12.2015": 500,
+			"23.8.2016":  100,
+		},
+	}
+	fmt.Println(list(t))
 }
