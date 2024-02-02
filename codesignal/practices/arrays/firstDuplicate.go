664d4840eab0962575aaf0642d1e914134fc848e
Josiah <josiah.connors23@outlook.com>
2024-02-02 10:04:31 -0500

    init not repeating 1 tc firstDup incomplete

--- codesignal/practices/arrays/firstDuplicate.go
+++ codesignal/practices/arrays/firstDuplicate.go
@@ -15,6 +15,12 @@ For a = [2, 1, 3, 5, 3, 2], the output should be solution(a) = 3.
 
 // edge - >2 dupes, edge - > 3 of a kind or more
 
+// unpassed case:
+ [8, 4, 6, 2, 6, 4, 7, 9, 5, 8]
+
+ e 6 r 4
+
+
 */
 
 package main
