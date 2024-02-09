6b297b01bd29c3fa6c3f56f4f6c4a3c535014eb6
Josiah Connors <josiah.connors23@outlook.com>
2024-02-09 17:52:14 -0500

    added initial account struct for method, new configs needed to account for payments struct

--- katas/spending/spending.go
+++ katas/spending/spending.go
@@ -2,12 +2,11 @@ package main
 
 import "fmt"
 
-type User struct {
-	Name     string
-	Payments map[string]Payment
+type Accounts struct {
+	Users map[string]Payments
 }
 
-type Payment struct {
+type Payments struct {
 	price    float64
 	category string
 	date     string
@@ -16,9 +15,25 @@ type Payment struct {
 // iterate filter if payment = x category & y date then increment sum for comparisons
 
 func main() {
-	fmt.Println(spendingAlert())
+	// format account
+	// need to find way to have multiple payments for each user and filter towards them interface?
+	accounts := Accounts{
+		Users: map[string]Payments{
+			"Josh": Payments{
+				12.50, "entertainment", "2006-01-02",
+			},
+			"Charlie": Payments{
+				10.00, "food", "2008-04-07",
+			},
+		},
+	}
+	fmt.Println(spendingAlert(&accounts))
 }
 
-func spendingAlert() {
+func spendingAlert(a *Accounts) {
 
 }
+
+// func getUser(name string, Account *Accounts) User {
+// 	return Account.Users[name]
+// }
