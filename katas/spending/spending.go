42ed17fa9fcaeb9ee0b8fb11fa7a6fe10ce5ce04
Josiah Connors <josiah.connors23@outlook.com>
2024-02-14 13:43:27 -0500

    base struct for transactions, ready for filtering logic

--- katas/spending/spending.go
+++ katas/spending/spending.go
@@ -3,35 +3,44 @@ package main
 import "fmt"
 
 type Accounts struct {
-	Users map[string]Payments
+	Users map[int][]Transactions
 }
 
-type Payments struct {
+type Transactions struct {
 	price    float64
 	category string
 	date     string
 }
 
-// iterate filter if payment = x category & y date then increment sum for comparisons
+// iterate filter if Transaction = x category & y date then increment sum for comparisons
 
 func main() {
 	// format account
-	// need to find way to have multiple payments for each user and filter towards them interface?
+	// need to find way to have multiple Transactions for each user and filter towards them interface?
 	accounts := Accounts{
-		Users: map[string]Payments{
-			"Josh": Payments{
-				12.50, "entertainment", "2006-01-02",
+		Users: map[int][]Transactions{
+			12389: {
+				{12.50, "entertainment", "2006-01-02"},
+				{14.78, "entertainment", "2006-09-02"},
+				{1.50, "food", "2006-05-02"},
 			},
-			"Charlie": Payments{
-				10.00, "food", "2008-04-07",
+			124673: {
+				{12.50, "entertainment", "2006-01-02"},
+				{14.78, "entertainment", "2006-09-02"},
+				{1.50, "food", "2006-05-02"},
 			},
 		},
 	}
-	fmt.Println(spendingAlert(&accounts))
+	fmt.Println(spendingAlert(&accounts, 12389))
 }
 
-func spendingAlert(a *Accounts) {
+func spendingAlert(a *Accounts, id int) bool {
+	//var current_month, previous_month := float64
+	for _, transaction := range a.Users[id] {
 
+	}
+
+	return true
 }
 
 // func getUser(name string, Account *Accounts) User {
