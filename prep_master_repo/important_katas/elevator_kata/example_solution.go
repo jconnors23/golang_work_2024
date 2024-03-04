5ae16a1ff32f2656a8d3b83a1c1a0344a62d9604
Josiah Connors <josiah.connors23@outlook.com>
2024-03-04 15:21:14 -0500

    elevator comments

--- prep_master_repo/important_katas/elevator_kata/example_solution.go
+++ prep_master_repo/important_katas/elevator_kata/example_solution.go
@@ -1,4 +1,10 @@
-// simulate an elevator software system
+/*
+ Open ended design for this Kata. This design features:
+- queue for floors
+- elevator goes to closest relative floor
+- direction arrows, UI features, and user prompts
+- os exit
+*/
 
 package main
 
@@ -13,14 +19,14 @@ import (
 func main() {
 	elevator := Elevator{Floor: 1}
 	fmt.Printf("Base Floor: 1\n")
-	floor_selection(&elevator)
-	sort_by_distance(&elevator)
+	floor_selection(&elevator)  // user inputs floors
+	sort_by_distance(&elevator) // sort floors based on distance from elevator current floor
 	for len(elevator.Queue) > 0 {
 		ascend_descend(&elevator)
 		// prevent index out of range
 		if len(elevator.Queue) == 1 {
 			ding(&elevator, elevator.Queue[0])
-			floor_selection(&elevator)
+			floor_selection(&elevator) // user can input more floors
 			sort_by_distance(&elevator)
 			// if no additional floors entered
 			if len(elevator.Queue) == 1 {
@@ -34,6 +40,7 @@ func main() {
 	}
 }
 
+// elevator will have current floor and Queue of floors attributes
 type Elevator struct {
 	Floor int
 	Queue []int
@@ -46,7 +53,8 @@ func floor_selection(e *Elevator) {
 		{4, 5, 6},
 		{7, 8, 9},
 	}
-	fmt.Printf("Select floor(s) (0 to stop):\n%d\n ", floors) // needs scanning go routine
+	fmt.Printf("Select floor(s) (0 to stop):\n%d\n ", floors)
+	// while loop, when the user selects 0 loop breaks, otherwise floors are added to slice
 	for {
 		var button int
 		fmt.Scanln(&button)
@@ -57,13 +65,14 @@ func floor_selection(e *Elevator) {
 			fmt.Printf("%d\n", floor_selections)
 		}
 	}
+	// enter each floor into the queue using helper method
 	for _, v := range floor_selections {
 		enqueue(e, v)
 	}
 }
 
 func enqueue(e *Elevator, desired_floor int) {
-	to_queue := desired_floor
+	to_queue := desired_floor // var is here to enhance readability
 	if to_queue < 1 || to_queue > 10 {
 		stop()
 	} else if contains(e, to_queue) {
@@ -73,6 +82,7 @@ func enqueue(e *Elevator, desired_floor int) {
 	}
 }
 
+// helper function to simulate if elevator floor has been chosen already
 func contains(e *Elevator, to_queue int) bool {
 	for _, floor := range e.Queue {
 		if floor == to_queue {
@@ -83,6 +93,7 @@ func contains(e *Elevator, to_queue int) bool {
 }
 
 func ascend_descend(e *Elevator) {
+	// print ascend, descend arrows
 	if e.Queue[0] > e.Floor {
 		arrow := "\u2191"
 		fmt.Printf("Ascending %v \n", arrow)
@@ -95,21 +106,22 @@ func ascend_descend(e *Elevator) {
 	// each floor will take 2 units of time to ascend / descend to
 	trip_time := int(distance * 2)
 	for trip_time > 0 {
-		time.Sleep(time.Second) // mimic elevator ascending/descending
+		time.Sleep(time.Second) // mimic elevator travel time using time.Sleep
 		trip_time -= 2
 	}
 }
 
+// ui
 func ding(e *Elevator, current int) {
 	e.Floor = current // floor after movement
 	fmt.Printf("'Ding'. %v floor\n", current)
 }
 
-// sort our queue by distance to current floor
+// sort our queue based on distance to current floor
 func sort_by_distance(e *Elevator) {
-	distace_to := e.Queue[0]
+	distance_to := e.Queue[0]
 	sort.Slice(e.Queue, func(i, j int) bool {
-		return absolute_value(distace_to, e.Queue[i]) < absolute_value(distace_to, e.Queue[j])
+		return absolute_value(distance_to, e.Queue[i]) < absolute_value(distance_to, e.Queue[j])
 	})
 	fmt.Println(e.Queue)
 }
@@ -118,16 +130,9 @@ func absolute_value(x, y int) float64 {
 	return math.Abs(float64(x) - float64(y))
 }
 
+// stop the elevator and exit the program
 func stop() {
 	fmt.Printf("Base Floor: 1\n")
 	fmt.Println("No floors queued or service unavailable.")
 	os.Exit(0)
 }
-
-/*
-future potential features:
-able to enter floors always - go routines?
-beep funcionality & show current floor as we ascend / descend each floor - fmt.Printf("'Beep'. %v floor\n", e.Floor)
-firealarm logic, call stop when user pulls firealarm
-print floors as grid in floor_selection
-*/
