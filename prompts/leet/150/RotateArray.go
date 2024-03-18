25dc53ccae9b3e6584467e9e02ac416ed647b923
Josiah Connors <josiah.connors23@outlook.com>
2024-03-18 17:52:15 -0400

    incomplete mock, partial kata

---
+++ prompts/leet/150/RotateArray.go
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/rotate-array/description/?envType=study-plan-v2&envId=top-interview-150
+
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 2, 3, 4, 5, 6, 7}
+	k := 3
+	fmt.Println(nums, k)
+}
+
+func rotate(nums []int, k int) []int {
+	return nums
+}
