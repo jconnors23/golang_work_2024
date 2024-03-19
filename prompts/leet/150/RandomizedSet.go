d5231996fa8f36ee35a665fa8de52c7dae50ee77
Josiah Connors <josiah.connors23@outlook.com>
2024-03-18 21:01:21 -0400

    init random set

--- prompts/leet/150/RandomizedSet.go
+++ prompts/leet/150/RandomizedSet.go
@@ -1,28 +1,65 @@
 package main
 
+import (
+	"math/rand"
+	"sort"
+)
+
 //https://leetcode.com/problems/insert-delete-getrandom-o1/description/?envType=study-plan-v2&envId=top-interview-150
+
+// stack behavior
 
 func main() {
 
 }
 
 type RandomizedSet struct {
+	set []int
 }
 
 func Constructor() RandomizedSet {
-
+	new_set := RandomizedSet{
+		set: []int{},
+	}
+	return new_set
 }
 
 func (this *RandomizedSet) Insert(val int) bool {
+	if val > this.Peep() {
+		this.set = append(this.set, val)
+		return true
+	}
+	for _, v := range this.set {
+		if v == val {
+			return false
+		}
+	}
+	this.set = append(this.set, val)
+	return true
+}
 
+func (this *RandomizedSet) Peep() int {
+	return this.set[0]
+}
+
+func (this *RandomizedSet) Stack() {
+	sort.Ints(this.set)
 }
 
 func (this *RandomizedSet) Remove(val int) bool {
-
+	for index, v := range this.set {
+		if v == val {
+			this.set = append(this.set[:index], this.set[index+1:]...)
+			this.Stack()
+			return true
+		}
+	}
+	return false
 }
 
 func (this *RandomizedSet) GetRandom() int {
-
+	randomIndex := rand.Intn(len(this.set))
+	return this.set[randomIndex]
 }
 
 /**
